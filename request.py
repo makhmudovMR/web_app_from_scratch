ecf28322599e44fd29fb96b852290868dc015c4f
Magomed Makhmudov <mmriadovich@gmail.com>
2018-08-15 12:50:02 +0300

    rename request_class -> request and delete service.py

--- request.py
+++ request.py
@@ -1,14 +1,20 @@
 import typing
 import socket
 from service import iter_lines
+from headers import Headers
+from bodyreader_class import BodyReader
 
 
 class Request:
+    '''
+    This class must be understanded
+    '''
 
     def __init__(self, *args, **kwargs):
         self.method = kwargs['method']
         self.path = kwargs['path']
         self.headers = kwargs['headers']
+        self.body = kwargs['body']
 
     @classmethod
     def from_socket(cls, sock: socket.socket) -> "Request":
@@ -26,12 +32,21 @@ class Request:
             raise ValueError(f"Недопустимая строка  {request_line}")
 
 
-        headers = {}
+        headers = Headers()
+        buff = b""
+        while True:
+            try:
+                line = next(lines)
+            except StopIteration as e:
+                # StopIteration.value contains the return value of the generator.
+                buff = e.value
+                break
 
-        for line in lines:
             try:
                 name, _, value = line.decode("ascii").partition(":")
-                headers[name.lower()] = value.lstrip()
+                headers.add(name, value.lstrip())
             except ValueError:
-                raise ValueError(f"Недопустимая строка заголовка {line}")
-        return cls(method=method.upper(), path=path, headers=headers)
+                raise ValueError(f"Malformed header line {line!r}.")
+
+        body = BodyReader(sock, buff=buff)
+        return cls(method=method.upper(), path=path, headers=headers, body=body)
