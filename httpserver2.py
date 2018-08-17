25fa1a1879bc0776a33d3bd4d25deda100e7223c
Magomed Makhmudov <mmriadovich@gmail.com>
2018-08-17 22:11:45 +0300

    httpserver and httpworker(thread | поток)

---
+++ httpserver2.py
@@ -0,0 +1,38 @@
+import typing
+import socket
+from queue import Queue
+from httpworker import HTTPWorker
+
+
+class HTTPServer:
+    def __init__(self, host="127.0.0.1", port=9000, worker_count=16) -> None:
+        self.host = host
+        self.port = port
+        self.worker_count = worker_count
+        self.worker_backlog = worker_count * 8
+        self.connection_queue = Queue(self.worker_backlog)
+
+    def serve_forever(self):
+        workers = []
+        for _ in range(self.worker_count):
+            worker = HTTPWorker(self.connection_queue)
+            worker.start()
+            workers.append(worker)
+
+        with socket.socket() as server_sock:
+            server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            server_sock.bind((self.host, self.port))
+            server_sock.listen(self.worker_backlog)
+            print(f"Listening on {self.host}:{self.port}...")
+
+            while True:
+                try:
+                    self.connection_queue.put(server_sock.accept())
+                except KeyboardInterrupt:
+                    break
+
+        for worker in workers:
+            worker.stop()
+
+        for worker in workers:
+            worker.join(timeout=30)
