081b091a743b6cd6721d057de895c922e07586c6
Magomed Makhmudov <mmriadovich@gmail.com>
2018-08-19 18:25:16 +0300

    simple_server

---
+++ simple_server.py
@@ -0,0 +1,24 @@
+import socket
+
+HOST = "127.0.0.1"
+PORT = 9000
+
+
+RESPONSE = b"""\
+HTTP/1.1 200 OK
+Content-type: text/html
+Content-length: 15
+
+<h1>Hello!</h1>""".replace(b"\n", b"\r\n")
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_sock:
+    server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_sock.bind((HOST, PORT))
+    server_sock.listen(0)
+    print(f"Listening on {HOST}:{PORT}...")
+
+    while True:
+        client_sock, client_addr = server_sock.accept()
+        print(f"New connection from {client_addr}.")
+        with client_sock:
+            client_sock.sendall(RESPONSE)
